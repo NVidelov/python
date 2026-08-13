c91684f657c01a97c04ec1ecd8adc0d70b2fe2ab
Niko Videlov <nikolayvidelov@gmail.com>
2026-08-13 08:13:26 -0500

    day 8

--- 30DaysPython/day_8/day_8.py
+++ 30DaysPython/day_8/day_8.py
@@ -6,3 +6,28 @@ dog['breed'] = 'Corgi'
 dog['legs'] = 'short'
 dog['age'] = 3
 print(dog)
+
+student = {
+    'first_name': 'Niko',
+    'last_name': 'Vee',
+    'gender': 'Male',
+    'age': 37,
+    'marital_status': 'In a relationship',
+    'skills': ['Python', 'PowerShell'],
+    'country': 'Spain',
+    'city': 'Madrid'
+}
+
+print(len(student))
+print(student['skills'])
+print(type(student['skills']))
+student['skills'].append('SQL')
+print(student['skills'])
+
+print(student.keys())
+print(student.values())
+
+print(student.items())
+
+student.pop('marital_status')
+del dog
